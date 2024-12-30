f138fac4bed1e992f4e3d0f535ea71622be6b893
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-12-30 19:53:11 +0530

    Add logout command

--- cli/command/auth/logout.go
+++ cli/command/auth/logout.go
@@ -1,8 +1,10 @@
 package auth
 
 import (
+	"fmt"
 	"wpm/cli/command"
 
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -10,12 +12,26 @@ func NewLogoutCommand(wpmCli command.Cli) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "logout",
 		Short: "Log out from the wpm registry",
-		RunE:  func(cmd *cobra.Command, args []string) error { return runLogout(wpmCli, cmd, args) },
+		RunE:  func(cmd *cobra.Command, args []string) error { return runLogout(wpmCli) },
 	}
 
 	return cmd
 }
 
-func runLogout(wpmCli command.Cli, cmd *cobra.Command, args []string) error {
+func runLogout(wpmCli command.Cli) error {
+	cfg := wpmCli.ConfigFile()
+
+	if cfg.AuthToken == "" {
+		return errors.Errorf("user must be logged in to perform this action")
+	}
+
+	cfg.AuthToken = ""
+
+	if err := cfg.Save(); err != nil {
+		return err
+	}
+
+	fmt.Fprintf(wpmCli.Out(), "user logged out successfully\n")
+
 	return nil
 }
