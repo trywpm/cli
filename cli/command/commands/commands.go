0b64624107b600eb143d31bb7e0a3fe165a7aa24
thelovekesh <lovekesh.kumar@rtcamp.com>
2025-01-11 22:29:30 +0530

    Add new publish command

--- cli/command/commands/commands.go
+++ cli/command/commands/commands.go
@@ -4,6 +4,7 @@ import (
 	"wpm/cli/command"
 	"wpm/cli/command/auth"
 	pmInit "wpm/cli/command/init"
+	"wpm/cli/command/publish"
 	"wpm/cli/command/whoami"
 
 	"github.com/spf13/cobra"
@@ -14,5 +15,6 @@ func AddCommands(cmd *cobra.Command, wpmCli command.Cli) {
 		auth.NewAuthCommand(wpmCli),
 		pmInit.NewInitCommand(wpmCli),
 		whoami.NewWhoamiCommand(wpmCli),
+		publish.NewPublishCommand(wpmCli),
 	)
 }
