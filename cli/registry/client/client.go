e1502630f847da9e8b3f95bd428b32195706d5da
thelovekesh <lovekesh.kumar@rtcamp.com>
2025-06-09 14:53:10 +0530

    Add UploadTarball method in registry client

--- cli/registry/client/client.go
+++ cli/registry/client/client.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"io"
 
 	"wpm/pkg/api"
 	"wpm/pkg/streams"
@@ -20,9 +21,8 @@ type client struct {
 // RegistryClient is a client used to communicate with a wpm distribution
 // registry
 type RegistryClient interface {
-	GetTarball(ctx context.Context, name string) (string, error)
-	GetPackage(ctx context.Context, name string) (wpm.Config, error)
-	PutPackage(ctx context.Context, data *wpm.Package) (string, error)
+	PublishPackage(ctx context.Context, data *wpm.Package, requestId string) (string, error)
+	UploadTarball(ctx context.Context, body io.Reader, opts UploadTarballOptions) (UploadTarballResponse, error)
 }
 
 var _ RegistryClient = &client{}
@@ -47,22 +47,47 @@ func NewRegistryClient(host string, authToken string, userAgent string, out *str
 	}, nil
 }
 
-func (c *client) GetTarball(ctx context.Context, name string) (string, error) {
-	return "", nil
+// UploadTarballOptions defines the options for uploading a tarball to the registry.
+type UploadTarballOptions struct {
+	Name    string // package name
+	Acl     string // must be one of "public" or "private"
+	Digest  string // base64 encoded digest of the package
+	Version string // package version
 }
 
-func (c *client) GetPackage(ctx context.Context, name string) (wpm.Config, error) {
-	return wpm.Config{}, nil
+// UploadTarballResponse defines the response structure for uploading a package.
+type UploadTarballResponse struct {
+	ID      string `json:"id"`
+	Message string `json:"message"`
 }
 
-func (c *client) PutPackage(ctx context.Context, data *wpm.Package) (string, error) {
+func (c *client) UploadTarball(ctx context.Context, body io.Reader, opts UploadTarballOptions) (UploadTarballResponse, error) {
+	response := UploadTarballResponse{}
+	err := c.restClient.Put(
+		"/"+opts.Name+"/"+opts.Version,
+		body,
+		&response,
+		api.WithHeader("x-wpm-acl", opts.Acl),
+		api.WithHeader("x-wpm-checksum-sha256", opts.Digest),
+		api.WithHeader("Content-Type", "application/octet-stream"),
+	)
+	if err != nil {
+		return UploadTarballResponse{}, err
+	}
+
+	return response, nil
+}
+
+// PublishPackage publishes a package to the registry.
+// It requires the package data and a request ID from the upload tarball request.
+func (c *client) PublishPackage(ctx context.Context, data *wpm.Package, requestId string) (string, error) {
 	bodyBytes, err := json.Marshal(data)
 	if err != nil {
 		return "", err
 	}
 
 	response := struct{ Message string }{}
-	err = c.restClient.Put("/", bytes.NewReader(bodyBytes), &response)
+	err = c.restClient.Put("/", bytes.NewReader(bodyBytes), &response, api.WithHeader("x-wpm-req-id", requestId))
 	if err != nil {
 		return "", err
 	}
