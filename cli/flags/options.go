2005b7f179e5e5c477034ca0b87d284f4ba94dcd
thelovekesh <lovekesh.kumar@rtcamp.com>
2025-06-09 10:44:43 +0530

    Add registry global option

--- cli/flags/options.go
+++ cli/flags/options.go
@@ -17,6 +17,7 @@ type ClientOptions struct {
 	LogLevel  string
 	ConfigDir string
 	Cwd       string
+	Registry  string
 }
 
 // NewClientOptions returns a new ClientOptions.
@@ -27,10 +28,12 @@ func NewClientOptions() *ClientOptions {
 // InstallFlags adds flags for the common options on the FlagSet
 func (o *ClientOptions) InstallFlags(flags *pflag.FlagSet) {
 	configDir := config.Dir()
+	defaultRegistry := "registry.wpm.so"
 
 	flags.BoolVarP(&o.Debug, "debug", "D", false, "Enable debug mode")
 	flags.StringVar(&o.Cwd, "cwd", "", "Set the current working directory")
 	flags.StringVar(&o.ConfigDir, "config", configDir, "Location of client config files")
+	flags.StringVar(&o.Registry, "registry", defaultRegistry, "Set specific registry to use")
 	flags.StringVarP(&o.LogLevel, "log-level", "l", "info", `Set the logging level ("debug", "info", "warn", "error", "fatal")`)
 }
 
