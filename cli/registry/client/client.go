a0b1489a7c9ca9d68d9ec5f99fba43505c372058
thelovekesh <lovekesh.kumar@rtcamp.com>
2025-12-29 01:22:39 +0530

    Update registry client

--- cli/registry/client/client.go
+++ cli/registry/client/client.go
@@ -1,10 +1,9 @@
 package client
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
+	"io"
 
 	"wpm/pkg/api"
 	"wpm/pkg/pm/wpmjson"
@@ -21,8 +20,7 @@ type client struct {
 // RegistryClient is a client used to communicate with a wpm distribution
 // registry
 type RegistryClient interface {
-	PublishPackage(ctx context.Context, data *wpmjson.Package, opts PublishPackageOptions) (string, error)
-	GetUploadTarballUrl(ctx context.Context, opts UploadTarballOptions) (UploadTarballResponse, error)
+	PutPackage(ctx context.Context, data *wpmjson.Package, tarball io.Reader) (string, error)
 }
 
 var _ RegistryClient = &client{}
@@ -47,71 +45,20 @@ func NewRegistryClient(host string, authToken string, userAgent string, out *str
 	}, nil
 }
 
-// UploadTarballOptions defines the options for uploading a tarball to the registry.
-type UploadTarballOptions struct {
-	Name           string // package name
-	Acl            string // must be one of "public" or "private"
-	Digest         string // base64 encoded digest of the package
-	Version        string // package version
-	Type           string // package type, e.g., "theme", "plugin" or "mu-plugin"
-	ContentLength  int64  // length of the content being uploaded
-	IdempotencyKey string // unique key to ensure idempotent uploads
-}
-
-// UploadTarballResponse defines the response structure for uploading a package.
-type UploadTarballResponse struct {
-	Id  *string `json:"id"`
-	Url *string `json:"url"`
-
-	// In case of idempotent requests, the message field may contain information.
-	Message *string `json:"message,omitempty"`
-}
-
-func (c *client) GetUploadTarballUrl(ctx context.Context, opts UploadTarballOptions) (UploadTarballResponse, error) {
-	var response UploadTarballResponse
-	err := c.restClient.Post(
-		fmt.Sprintf("/%s/%s.tar.zst", opts.Name, opts.Version),
-		nil,
-		&response,
-		api.WithHeader("x-wpm-acl", opts.Acl),
-		api.WithHeader("x-wpm-package-type", opts.Type),
-		api.WithHeader("x-wpm-checksum-sha256", opts.Digest),
-		api.WithHeader("content-type", "application/octet-stream"),
-		api.WithHeader("x-wpm-idempotency-key", opts.IdempotencyKey),
-		api.WithHeader("x-wpm-content-length", fmt.Sprintf("%d", opts.ContentLength)),
-	)
-	if err != nil {
-		return UploadTarballResponse{}, err
-	}
-
-	return response, nil
-}
-
-// PublishPackageOptions defines the options for publishing a package to the registry.
-type PublishPackageOptions struct {
-	Name           string // package name
-	Version        string // package version
-	RequestId      string // request ID from the upload tarball request
-	IdempotencyKey string // unique key to ensure idempotent publishing
-}
-
-// PublishPackage publishes a package to the registry.
-// It requires the package data and a request ID from the upload tarball request.
-func (c *client) PublishPackage(ctx context.Context, data *wpmjson.Package, opts PublishPackageOptions) (string, error) {
-	bodyBytes, err := json.Marshal(data)
+// PutPackage uploads a package to the registry
+func (c *client) PutPackage(ctx context.Context, data *wpmjson.Package, tarball io.Reader) (string, error) {
+	manifest, err := json.Marshal(data)
 	if err != nil {
 		return "", err
 	}
 
 	response := struct{ Message string }{}
-	err = c.restClient.Put(
-		"/"+opts.Name+"/"+opts.Version,
-		bytes.NewReader(bodyBytes),
+	if err := c.restClient.Put(
+		"/",
+		tarball,
 		&response,
-		api.WithHeader("x-wpm-req-id", opts.RequestId),
-		api.WithHeader("x-wpm-idempotency-key", opts.IdempotencyKey),
-	)
-	if err != nil {
+		api.WithHeader("x-wpm-manifest", string(manifest)),
+	); err != nil {
 		return "", err
 	}
 
