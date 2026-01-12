4088967ea4c4ecf8f11f82c9240865c9ae456ddb
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-01-12 19:28:25 +0530

    Update keys in wpmjson config

--- pkg/pm/wpmjson/package.go
+++ pkg/pm/wpmjson/package.go
@@ -49,9 +49,11 @@ type Dist struct {
 
 // PackageConfig struct to define the package configuration
 type PackageConfig struct {
-	BinDir         *string `json:"bin-dir,omitempty"`
-	ContentDir     *string `json:"content-dir,omitempty"`
-	PlatformStrict *bool   `json:"platform-strict,omitempty"` // If set to true, wpm will refuse to install the package if the platform requirements are not met.
+	BinDir        string `json:"bin-dir,omitempty"`
+	ContentDir    string `json:"content-dir,omitempty"`
+	RuntimeStrict *bool  `json:"runtime-strict,omitempty"`
+	RuntimeWp     string `json:"runtime-wp,omitempty"`
+	RuntimePhp    string `json:"runtime-php,omitempty"`
 }
 
 // Requires holds wp and php version constraints for a package
@@ -60,25 +62,27 @@ type Requires struct {
 	PHP string `json:"php,omitempty" validate:"omitempty,wpm_semver_constraint"`
 }
 
+type Bin map[string]string
+type Scripts map[string]string
 type Dependencies map[string]string
 
 // Config struct to define the wpm.json schema
 type Config struct {
-	Name            string             `json:"name" validate:"required,wpm_name"`
-	Description     string             `json:"description,omitempty" validate:"omitempty,min=3,max=512"`
-	Private         bool               `json:"private,omitempty"`
-	Type            PackageType        `json:"type" validate:"required,oneof=theme plugin mu-plugin"`
-	Version         string             `json:"version" validate:"required,wpm_semver"`
-	Requires        *Requires          `json:"requires,omitempty" validate:"omitempty"`
-	License         string             `json:"license,omitempty" validate:"omitempty,min=3,max=100"`
-	Homepage        string             `json:"homepage,omitempty" validate:"omitempty,url,wpm_http_url,min=10,max=200"`
-	Tags            []string           `json:"tags,omitempty" validate:"omitempty,max=5,dive,min=2,max=64"`
-	Team            []string           `json:"team,omitempty" validate:"omitempty,max=100,dive,min=2,max=100"`
-	Bin             *map[string]string `json:"bin,omitempty"`
-	Dependencies    *Dependencies      `json:"dependencies,omitempty" validate:"omitempty,max=16,dive,keys,wpm_name,endkeys,wpm_dependency_version"`
-	DevDependencies *Dependencies      `json:"devDependencies,omitempty" validate:"omitempty,max=16,dive,keys,wpm_name,endkeys,wpm_dependency_version"`
-	Scripts         *map[string]string `json:"scripts,omitempty"`
-	Config          *PackageConfig     `json:"config,omitempty"`
+	Name            string         `json:"name" validate:"required,wpm_name"`
+	Description     string         `json:"description,omitempty" validate:"omitempty,min=3,max=512"`
+	Private         bool           `json:"private,omitempty"`
+	Type            PackageType    `json:"type" validate:"required,oneof=theme plugin mu-plugin"`
+	Version         string         `json:"version" validate:"required,wpm_semver"`
+	Requires        *Requires      `json:"requires,omitempty" validate:"omitempty"`
+	License         string         `json:"license,omitempty" validate:"omitempty,min=3,max=100"`
+	Homepage        string         `json:"homepage,omitempty" validate:"omitempty,url,wpm_http_url,min=10,max=200"`
+	Tags            []string       `json:"tags,omitempty" validate:"omitempty,max=5,dive,min=2,max=64"`
+	Team            []string       `json:"team,omitempty" validate:"omitempty,max=100,dive,min=2,max=100"`
+	Bin             *Bin           `json:"bin,omitempty"`
+	Dependencies    *Dependencies  `json:"dependencies,omitempty" validate:"omitempty,max=16,dive,keys,wpm_name,endkeys,wpm_dependency_version"`
+	DevDependencies *Dependencies  `json:"devDependencies,omitempty" validate:"omitempty,max=16,dive,keys,wpm_name,endkeys,wpm_dependency_version"`
+	Scripts         *Scripts       `json:"scripts,omitempty"`
+	Config          *PackageConfig `json:"config,omitempty"`
 }
 
 // Description of package fields.
@@ -107,6 +111,7 @@ type PackageManifest struct {
 	Description     string            `json:"description,omitempty"`
 	Type            PackageType       `json:"type"`
 	Version         string            `json:"version"`
+	Bin             *Bin              `json:"bin,omitempty"`
 	Requires        *Requires         `json:"requires,omitempty"`
 	License         string            `json:"license,omitempty"`
 	Homepage        string            `json:"homepage,omitempty"`
