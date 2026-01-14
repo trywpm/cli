4f554f9e59745002f65b0a8578bf1a0fc09f93c0
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-01-14 21:18:53 +0530

    Make output helpers standalone

--- pkg/output/output.go
+++ pkg/output/output.go
@@ -27,24 +27,24 @@ type Text struct {
 	Fancy string
 }
 
-func (o *Output) Prettyln(t Text) (int, error) {
+func (o *Output) Prettyln(t Text) {
 	if o.out.IsColorEnabled() {
-		return o.out.WriteString(t.Fancy + "\n")
+		_, _ = o.out.WriteString(t.Fancy + "\n")
 	}
-	return o.out.WriteString(t.Plain + "\n")
+	_, _ = o.out.WriteString(t.Plain + "\n")
 }
 
-func (o *Output) PrettyErrorln(t Text) (int, error) {
+func (o *Output) PrettyErrorln(t Text) {
 	if o.err.IsColorEnabled() {
-		return o.err.WriteString(t.Fancy + "\n")
+		_, _ = o.err.WriteString(t.Fancy + "\n")
 	}
-	return o.err.WriteString(t.Plain + "\n")
+	_, _ = o.err.WriteString(t.Plain + "\n")
 }
 
-func (o *Output) Write(s string) (int, error) {
-	return o.out.WriteString(s)
+func (o *Output) Write(s string) {
+	_, _ = o.out.WriteString(s)
 }
 
-func (o *Output) ErrorWrite(s string) (int, error) {
-	return o.err.WriteString(s)
+func (o *Output) ErrorWrite(s string) {
+	_, _ = o.err.WriteString(s)
 }
