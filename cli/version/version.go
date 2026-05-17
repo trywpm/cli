c4c6cb24b5abf40e8d26aeb26d5e02dc5c14d82d
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-17 18:32:36 +0530

    Release: v0.1.12

--- cli/version/version.go
+++ cli/version/version.go
@@ -4,7 +4,7 @@ package version
 // These values are overridden via ldflags
 var (
 	PlatformName = ""
-	Version      = "unknown-version"
+	Version      = "0.1.12"
 	GitCommit    = "unknown-commit"
 	BuildTime    = "unknown-buildtime"
 )
